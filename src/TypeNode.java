ce044fd380a98c517ef9c8cfbe702ca2b263ac1d
greagob <ebattaglia929@gmail.com>
2024-09-27 19:51:04 -0400

    Stubbed out Bool, Operand, and Type nodes

---
+++ src/TypeNode.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+import provided.*;
+
+public class TypeNode implements JottTree {
+    public JottTree parse(ArrayList<Token> tokens) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'parse'");
+    }
+
+    @Override
+    public String convertToJott() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+    }
+
+    @Override
+    public boolean validateTree() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validateTree'");
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+}
