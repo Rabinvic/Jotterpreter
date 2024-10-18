b278bf54115937391d7ec7aa20c9a1ac3fed7f99
gar1ik <amr6559@rit.edu>
2024-10-18 02:24:02 -0400

    FBody, FuncDef, & Program

--- src/FBodyNode.java
+++ src/FBodyNode.java
@@ -9,17 +9,36 @@ public class FBodyNode implements JottTree {
     private ArrayList<Var_DecNode> vardecs;
     private BodyNode body;
 
-    public FBodyNode() {
+    public FBodyNode(ArrayList<Var_DecNode> vardecs, BodyNode body) {
         this.vardecs = vardecs;
         this.body = body;
     }
 
     public static FBodyNode parseFBodyNode(ArrayList<Token> tokens) {
         if(0==tokens.size()) {
-            System.err.print("Syntax Error:\n No tokens to parse\n");
+            System.err.println("Syntax Error:\n No tokens to parse");
             return null;
         }
         
+        // <var_dec>* <body>
+
+        // <var_dec>*
+        ArrayList<Var_DecNode> vardecs = new ArrayList<Var_DecNode>();
+        while(tokens.get(0).getTokenType() != TokenType.ID_KEYWORD) {
+            Var_DecNode vardec = Var_DecNode.parseVarDecNode(tokens);
+            if(vardec == null) {
+                return null;
+            }
+            vardecs.add(vardec);
+        }
+
+        // <body>
+        BodyNode body = BodyNode.parseBodyNode(tokens);
+        if(body == null) {
+            return null;
+        }
+
+        return new FBodyNode(vardecs, body);
     }
 
     public String convertToJott() {
--- src/ProgramNode.java
+++ src/ProgramNode.java
@@ -8,16 +8,27 @@ import provided.*;
 public class ProgramNode implements JottTree {
     private ArrayList<FunctionDefNode> funcdefs;
 
-    public ProgramNode() {
+    public ProgramNode(ArrayList<FunctionDefNode> funcdefs) {
         this.funcdefs = funcdefs;
     }
 
     public static ProgramNode parseProgramNode(ArrayList<Token> tokens) {
         if(0==tokens.size()) {
-            System.err.print("Syntax Error:\n No tokens to parse\n");
+            System.err.println("Syntax Error:\n No tokens to parse");
             return null;
         }
 
+        // <function_def>* <EOF>
+
+        ArrayList<FunctionDefNode> funcdefs = new ArrayList<FunctionDefNode>();
+        while(tokens.get(0).getTokenType() == TokenType.ID_KEYWORD && tokens.get(0).getToken() == "Def") {
+            FunctionDefNode funcdef = FunctionDefNode.parseFunctionDefNode(tokens);
+            if(funcdef == null) {
+                return null;
+            }
+            funcdefs.add(funcdef);
+        }
+        return new ProgramNode(funcdefs);
     }
 
     public String convertToJott() {
