2f4845fe403ef8a62bce9dc7b5efb08a12e2d809
greagob <ebattaglia929@gmail.com>
2024-10-24 17:40:35 -0400

    adjusted SymbolTable.java

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -1,5 +1,9 @@
 import java.util.HashMap;
 
 public class SymbolTable {
-    public static HashMap<String, String> symbolTable = new HashMap<String, String>();
+    // This just stores <Key, Value> pairs of <Function Name, Type>
+    public static HashMap<String, String> funcTypes = new HashMap<String, String>();
+
+    // This stores <Key, <Key, Value>> pairs of <Function Name, <Variable Name, Type>>
+    public static HashMap<String, HashMap<String,String>> funcTables = new HashMap<String, HashMap<String,String>>();
 }
